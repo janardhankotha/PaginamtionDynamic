2675f79fb3b3074018b5fa4313c4a3f36c12666d
Jana <janardhan.srit@gmail.com>
2018-03-22 13:16:13 +0530

    Initial commit

---
+++ app/src/main/java/com/paginamtiondynamic/Config.java
@@ -0,0 +1,15 @@
+package com.paginamtiondynamic;
+
+/**
+ * Created by G2evolution on 12/18/2017.
+ */
+
+public class Config {
+    //Data URL
+    public static final String DATA_URL = "http://www.g2evolution.in/g2e_sales/android/pagin.php?page=";
+
+    //JSON TAGS
+    public static final String TAG_IMAGE_URL = "image";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_PUBLISHER = "publisher";
+}
---
+++ app/src/main/java/com/paginamtiondynamic/SuperHero.java
@@ -0,0 +1,38 @@
+package com.paginamtiondynamic;
+
+/**
+ * Created by G2evolution on 12/18/2017.
+ */
+
+public class SuperHero {
+
+    //Data Variables
+    private String imageUrl;
+    private String name;
+    private String publisher;
+
+    //Getters and Setters
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
